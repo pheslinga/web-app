f4881ef602f9d4e22e799d4fa1fb92a5de061521
pheslinga <Pheslinga@gmail.com>
2016-04-17 21:32:58 +0000

    Initial Commit

---
+++ client/app/app.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('dashboardApp', [
+  'dashboardApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
